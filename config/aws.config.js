2627705ceaefc701d3fc9c94b97f0d72bbc85139
GouseAshwak <gouseashwak007@gmail.com>
2021-11-17 11:59:13 +0530

    chat

--- config/aws.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    AWS_ACCESS_KEY_ID : 'AKIARJ3BM2T2BSWBPWVI',
-    AWS_SECRET_ACCESS_KEY : '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',
-    AWS_REGION : 'ap-northeast-2',
-    AWS_BUCKET_NAME : 'qa-one-customer-doc',
-    AWS_Uploaded_File_URL_LINK : 'https://qa-one-customer-doc.s3.ap-northeast-2.amazonaws.com/'
-};
